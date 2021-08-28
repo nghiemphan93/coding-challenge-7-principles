4f3f6cf03307f0505ccd78a62c5f8f5d79fd90ea
Nghiem Phan <nghiemphan93@gmail.com>
2021-08-28 17:57:12 +0200

    first commit

---
+++ src/main/java/com/example/codingchallenge7principles/CodingChallenge7PrinciplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.codingchallenge7principles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingChallenge7PrinciplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingChallenge7PrinciplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codingchallenge7principles/CodingChallenge7PrinciplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codingchallenge7principles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingChallenge7PrinciplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
